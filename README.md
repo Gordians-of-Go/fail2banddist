15f5aa1ac7d9d6a61aacc134fdc49851cbb01202
fuster92 <fusti92@gmail.com>
2024-04-02 23:16:34 +0200

    Revert "feat(go): first commit"
    This reverts commit bbff592745dc0020a71e80d93b2f26358fd9c244.

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # fail2banddist
-
-## Precommit dependencies
-
-Golang utilities:
-* [golangci-lint](https://golangci-lint.run/welcome/install/), linter
--- go/pkg/response/dummy.go
+++
@@ -1,6 +0,0 @@
-package response
-
-// Dummy is a dummy response to test client-server communication
-type Dummy struct {
-	Message string `json:"message"`
-}
